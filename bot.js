02d2ab3b3c104afa57b62215efac5ecd047af0f6
Swizec Teller <swizec@swizec.com>
2012-06-07 01:25:42 +0200

    some basic stats are collecting

--- bot.js
+++ bot.js
@@ -1,21 +1,31 @@
 
 var irc = require('irc'),
+    graphite = require('graphite'),
     util = require('util');
 
-var client = new irc.Client('irc.freenode.net',
-                            '_a-bot_', {
+var irc_client = new irc.Client('irc.freenode.net',
+                                '_stats_', {
                                 channels: ['#psywerx']
                             });
+var graph_client = graphite.createClient('plaintext://localhost:2003');
 
-client.on('message#psywerx', function (nick, text, message) {
-    console.log(nick+' => '+text);
-    console.log(message);
+var url = /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi;
+
+irc_client.on('message#psywerx', function (nick, text, message) {
+    graph_client.write({message: 1,
+                        words: text.split(' ').length,
+                        links: text.split(url).length},
+                       function (err) {
+                           if (err) {
+                               console.log(err);
+                           }
+                        });
 });
 
-client.on('join#psywerx', function (nick, message) {
-    console.log(nick+' has joined');
+irc_client.on('join#psywerx', function (nick, message) {
+    graph_client.write({join: 1}, function (err) {});
 });
 
-client.on('part#psywerx', function (nick, message) {
-    console.log(nick+' has left');
+irc_client.on('part#psywerx', function (nick, message) {
+    graph_client.write({leave: 1}, function(err) {});
 });
